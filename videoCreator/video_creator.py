67f47a33c20ffad0faf979ea8d091e778abea4f7
Sujan <sujancode@gmail.com>
2022-12-08 15:22:29 -0500

    K xaa

--- videoCreator/video_creator.py
+++ videoCreator/video_creator.py
@@ -41,7 +41,7 @@ def create_video(video_path,overlay_path,output_path):
     home=CompositeVideoClip([clip,overlay])
 
     result=concatenate_videoclips([home,video])
-    # result.resize(height=480)
+    result.resize(height=480)
     result.write_videofile(output_path)
     return True
 
