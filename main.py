c6e4468c8a58e58309fa28a908243751d2d1b3f3
Sujan <sujancode@gmail.com>
2022-12-08 13:41:20 -0500

    Sup

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 from videoDownloader.functions import dl_start,add_check,check_for_database
-
+import sys
 def main():
     check_for_database()
-
-    url="https://www.pornhub.com/pornstar/rae-lil-black/videos"
+    url=sys.argv[1]
+    print(url)
     add_check(url)
     dl_start()
 main()
