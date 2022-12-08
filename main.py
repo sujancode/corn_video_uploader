56c747139fa49f76f7fe9d3ae1b55268d8858e7a
Sujan <sujancode@gmail.com>
2022-12-08 15:15:14 -0500

    Sup

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from videoDownloader.functions import dl_start,add_check,check_for_database
 import sys
 def main():
     check_for_database()
-    url=sys.argv[1]
-    print(url)
-    add_check(url)
+    if len(sys.argv)>=1:
+        url=sys.argv[1]
+        print(url)
+        add_check(url)
     dl_start()
 main()
